f927ff10a34086517aea05d3297efdf138a4e328
kiran <kiranpant9840@gmail.com>
2022-10-21 15:13:35 +0545

    bcrypt password comparison done

--- Server/src/Controller/loginRouter.js
+++ Server/src/Controller/loginRouter.js
@@ -1,14 +1,26 @@
 // post request for Login
-const express = require ("express")
-const router = express.Router()
-const login = require("../Model/loginSchema")
-const products= require("../Model/usersSchema")
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const login = require("../Model/loginSchema");
+const users = require("../Model/usersSchema");
 router.post("/", async (req, res) => {
-  const user = products.findOne(
-    {
-      phoneNumber:req.body.phoneNumber
-    })
-  console.log(user)
+  const userData = users.findOne({ phoneNumber: req.body.phoneNumber},
+ function (err, data) {
+      if (err) console.log(err)
+      if (data) {
+        bcrypt.compare(req.body.password, data.password, function (err, result) {
+        
+          if(err) console.log(err)
+          if(result){
+            res.json({
+              message:" password matched"
+            })
+          }
+        })
+      }
+    });
 });
 
-module.exports = router
+module.exports = router;
