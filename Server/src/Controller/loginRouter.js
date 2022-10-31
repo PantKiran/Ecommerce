ec206a39c6d9f2804957e5e1ea2ba50262602f7d
kiran <kiranpant9840@gmail.com>
2022-10-31 15:12:32 +0545

    jwt token matching

--- Server/src/Controller/loginRouter.js
+++ Server/src/Controller/loginRouter.js
@@ -1,6 +1,7 @@
 // post request for Login
 const express = require("express");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const login = require("../Model/loginSchema");
@@ -14,10 +15,13 @@ router.post("/", async (req, res) => {
         
           if(err) console.log(err)
           if(result){
+            const token = jwt.sign({user:req.body.user }, process.env.TOKEN_SECRET);
+             console.log(token)
             res.json({
-              message:" password matched"
+              message:" password matched",
+              token:token
             })
-          }
+          }  
         })
       }
     });
